f96164e45e896ecc69b9c1b52f5129df4a31a665
caizhenyu <296674048@qq.com>
2020-10-12 10:24:20 +0800

    ajax和页面完成

--- target/classes/js/bootstrap.bundle.js
+++ target/classes/js/bootstrap.bundle.js
@@ -4,8 +4,8 @@
   * Licensed under MIT (https://github.com/twbs/bootstrap/blob/master/LICENSE)
   */
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('jquery')) :
-  typeof define === 'function' && define.amd ? define(['exports', 'jquery'], factory) :
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('src/main/webapp/WEB-INF/js/jquery')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'src/main/webapp/WEB-INF/js/jquery'], factory) :
   (global = global || self, factory(global.bootstrap = {}, global.jQuery));
 }(this, (function (exports, $) { 'use strict';
 
